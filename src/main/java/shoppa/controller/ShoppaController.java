9209d436136c21eaa9328c3d853250f09e1ab2d9
Anthony Anyama <anthony.anyama@gmail.com>
2021-03-18 18:05:29 +0300

    Committing web files

--- src/main/java/shoppa/controller/ShoppaController.java
+++ src/main/java/shoppa/controller/ShoppaController.java
@@ -4,34 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import lombok.NoArgsConstructor;
 import shoppa.model.Item;
 
 //@NoArgsConstructor
 
 //@RequestMapping("items")
 //@RestController
-@Controller
+
 public class ShoppaController {
 	
-	UUID uuid;
-	
-	//@GetMapping("items/all")
-	
-	@RequestMapping(method = RequestMethod.GET, value = "items/all")
-	@ResponseBody
+   
 	public List<Item> getAllItems(){
 						
 		List<Item> items = new ArrayList<Item>();
-		uuid = UUID.randomUUID();
+		UUID uuid = UUID.randomUUID();
 		items.add(new Item(uuid,"Ugali", Double.parseDouble("200")));
 		items.add(new Item(uuid,"Fish", Double.parseDouble("100")));
 		items.add(new Item(uuid,"Eggs", Double.parseDouble("500")));
